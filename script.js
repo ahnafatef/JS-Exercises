148f0e04c5ecf281856c5bf475a2ce3ec3485085
ahnafatef <ahnafatefkhan@gmail.com>
2019-09-30 09:50:07 +0600

    Classwork3 Task 1 and 2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* JavaScript code here */
+
+
+function arraySum (arr) {
+	let sum = 0;
+
+	arr.forEach(function(item){
+		sum += item
+	})
+
+	return (multiplier) => { return sum * multiplier }
+}
+
+let sum = arraySum([1,2,3,5,6]);
+
+console.log(sum(4));
