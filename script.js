b143f6c2c0ce43797a5e9394f61a375921b9d794
ahnafatef <ahnafatefkhan@gmail.com>
2019-10-02 09:12:11 +0000

    Task 3

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ function arraySum (arr) {
 	return (multiplier) => { return sum * multiplier }
 }
 
-let sum = arraySum([1,2,3,5,6]);
+//let sum = arraySum([1,2,3,5,6]);
 
-console.log(sum(4));
+// console.log(sum(4));
+
+// task 3
+
+let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+let validateEmail = function (email){
+						if (validEmail.test(email))
+							return true;
+						return false;
+					}
+
+console.log(validateEmail('adfa@af.com'))
+
