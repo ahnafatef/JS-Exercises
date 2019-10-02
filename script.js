085f383d2bc13e157057607cd83b7b0ffefe98fb
ahnafatef <ahnafatefkhan@gmail.com>
2019-10-02 09:12:42 +0000

    Task 4

--- script.js
+++ script.js
@@ -25,5 +25,42 @@ let validateEmail = function (email){
 						return false;
 					}
 
-console.log(validateEmail('adfa@af.com'))
+//console.log(validateEmail('adfa@af.com'))
+
+
+// task 4
+
+
+let library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+
+
+let readStatus = function(arr, index){
+	len = arr.length
+	if (index < len) {
+		obj = arr[index];
+		console.log(obj.readingStatus);
+	}
+	else {
+		console.log('invalid index');	
+	}
+}
+
+readStatus(library, 2)
+
 
